47748d41d06be90f9798a47508a77e6fd8995fe1
enrique palomino <octepc.97@gmail.com>
2023-03-23 08:52:39 -0500

    feat:

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-red-300">
+      <h1 className="bg-red-300 w-full ">Hello world</h1>
+      <div className="text-center bg-red-600">
+        <h3>Hola</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
